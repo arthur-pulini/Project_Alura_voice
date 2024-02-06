b3406328b7b2c3f36a1397df4d747d626fc63098
Arthur Pulini <arthurpulini@gmail.com>
2024-02-06 19:13:28 -0300

    first commit

---
+++ Alura_Voice-Exercise.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+datas = pd.read_csv('/home/arthur-pulini/Documentos/Programação/Machine learning Alura/Alura_Voice-Exercise/Customer-Churn.csv')
+head = datas.head()
+print(head)
