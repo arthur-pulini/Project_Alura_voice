ecb710dbc540c857a886c78cc1f6bace46f35bc1
Arthur Pulini <arthurpulini@gmail.com>
2024-02-07 10:50:15 -0300

    Unbalanced_datase-applying_oversampling

--- Alura_Voice-Exercise.py
+++ Alura_Voice-Exercise.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
 
 datas = pd.read_csv('/home/arthur-pulini/Documentos/Programação/Machine learning Alura/Alura_Voice-Exercise/Customer-Churn.csv')
 head = datas.head()
@@ -21,3 +24,24 @@ print(head)
 finalDatas = pd.concat([modifiedData, dummieDatas], axis = 1)
 head = finalDatas.head()
 print(head)
+
+pd.set_option('display.max_columns', 39)
+head = finalDatas.head()
+print(head)
+
+xMaria = [[0,0,1,1,0,0,39.90,1,0,0,0,1,0,1,0,0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,0,0,1,0,0,0,1]]
+
+#ax = sns.countplot(x='Churn', data=finalDatas)
+#plt.show()
+
+X = finalDatas.drop('Churn', axis = 1)
+y = finalDatas['Churn']
+
+smt = SMOTE(random_state=123)
+X, y = smt.fit_resample(X, y)
+finalDatas = pd.concat([X, y], axis=1)
+head = finalDatas.head(2)
+print(head)
+
+ax = sns.countplot(x='Churn', data=finalDatas)
+plt.show()
